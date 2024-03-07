44957b2ca3305934c7e644430c48ed36348941f5
heakshaya <akshay.a@hubengage.com>
2024-03-07 18:29:19 +0530

    add flutter UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/post_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,13 +9,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const PostScreen();
+    /*return MaterialApp(
       title: 'HubEngage Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'HubEngage Demo Home Page'),
-    );
+    );*/
   }
 }
 
